fa0239ea030bfeeb4c88ac9c5eacaf7e612b1bea
oklave <vvalko2k20@gmail.com>
2024-03-29 21:50:09 +0400

    0.04 commit

--- sql/000001_create_init_tables.up.sql
+++ sql/000001_create_init_tables.up.sql
@@ -3,13 +3,11 @@
 SET TIMEZONE="Europe/Moscow";
 
 -- создать таблицу номеров
-CREATE TABLE books (
+CREATE TABLE numbers (
                      id UUID DEFAULT uuid_generate_v4 () PRIMARY KEY,
-                     created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW (),
-                     updated_at TIMESTAMP NULL,
-                     tg_id UUID NOT NULL REFERENCES users (id) ON DELETE CASCADE,
-                     title VARCHAR (255) NOT NULL,
-                     author VARCHAR (255) NOT NULL,
+                     phone_number VARCHAR (255) NOT NULL,
+                     tg_id UUID NOT NULL REFERENCES users (id) ON CASCADE,
+                     FIO VARCHAR (255) NOT NULL,
                      number_status INT NOT NULL
 );
 
